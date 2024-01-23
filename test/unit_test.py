9ac60f7c027e75bbd2753349086364f45a02793e
Akos Vagi <akos.vagi@gmail.com>
2024-01-23 23:35:22 +0100

    Unit tests for calculation module

--- test/unit_test.py
+++ test/unit_test.py
@@ -1,8 +1,28 @@
 import unittest
+import random
+import pandas as pd
+from homework.historic_prices import HistoricPricer
 
 
-class PricingTest(unittest.TestCase):
+class HistoricPricerForecast(unittest.TestCase):
 
-    def testing_works(self):
-        print('Unit testing works!')
-        self.assertTrue(True)
+    valid_ticker = 'AAPL'
+    invalid_ticker = 'sfsdfzs'
+
+    def setUp(self):
+        random.seed(10)
+        self.pricer = HistoricPricer()
+
+    def test_get_month_end_prices(self):
+        result = self.pricer.forecast(self.valid_ticker)
+        print(result)
+        self.assertGreaterEqual(result['max'], result['median'])
+        self.assertGreaterEqual(result['median'], result['min'])
+
+    def test_invalid_ticker(self):
+        result = self.pricer.forecast(self.invalid_ticker)
+        self.assertTrue('error' in result)
+
+    def test_get_historic_daily_prices(self):
+        result = self.pricer.get_historic_daily_prices(self.valid_ticker)
+        self.assertTrue(isinstance(result, pd.DataFrame))
