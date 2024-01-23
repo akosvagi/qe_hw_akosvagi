9a0e81da8e9cbc6f06e95a85f2852fd541ec0db4
Akos Vagi <akos.vagi@gmail.com>
2024-01-23 23:38:01 +0100

    Validation against JSON schema

---
+++ homework/app/validation.py
@@ -0,0 +1,50 @@
+import typing
+from jsonschema import validate
+
+
+class Validator:
+    """Base class for validating a JSON schema"""
+
+    schema: typing.Dict = {}
+
+    def __init__(self, instance):
+
+        validate(instance, self.schema)
+
+
+class HistoricPricerForecastParametersValidator(Validator):
+    """Parameter schema for GET request."""
+
+    schema = {
+        'type': 'object',
+        'required': ['ticker'],
+        'properties': {
+            'ticker': {
+                'type': 'string',
+            },
+            'num_of_scenarios': {
+                'type': 'integer',
+                'minimum': 1,
+                'maximum': 10000
+            },
+            'months': {
+                'type': 'integer',
+                'minimum': 1,
+                'maximum': 1200
+            }
+        }
+    }
+
+
+class HistoricPricerForecastResponseValidator(Validator):
+    """Parameter schema for response to GET request."""
+
+    schema = {
+        'type': 'object',
+        'required': ['min', 'median', 'max'],
+        'properties': {
+            'min': {'type': 'number'},
+            'median': {'type': 'number'},
+            'max': {'type': 'number'}
+        }
+    }
